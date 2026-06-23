fc532480a136cc5b6ff5e7348a594271fb57d827
Tom Felber <tom.felber@tu-dresden.de>
2026-06-23 18:40:10 +0200

    prepared ids for paths

--- hyrmir-lib/src/repository.rs
+++ hyrmir-lib/src/repository.rs
@@ -2,8 +2,6 @@ use crate::model::*;
 use crate::vcs::{VCSError, VCS};
 use crate::workspace::Workspace;
 use std::cell::RefCell;
-use std::error::Error;
-use std::ops::Deref;
 use std::rc::Rc;
 use thiserror::Error;
 
@@ -25,12 +23,13 @@ pub struct Repository<V: VCS> {
 impl<V: VCS> Repository<V> {
     fn scan_repository(&self) -> Result<(), ScanError<V::VCSError>> {
         let mut root = self.virtual_root.borrow_mut();
-        for path in self.get_vcs().iter_concrete_paths() {
-            let unwrapped = path?;
-            let p = if unwrapped.is_absolute() {
-                unwrapped.strip_n_left(1)
-            } else { unwrapped };
-            root.insert_path(&p, true)?;
+        for path_info in self.get_vcs().iter_concrete_paths() {
+            let unwrapped = path_info?;
+            let path = unwrapped.get_path();
+            let p = if path.is_absolute() {
+                &path.strip_n_left(1)
+            } else { path };
+            root.insert_path(p, true)?;
         }
         self.repo_scanned.replace(true);
         Ok(())
--- hyrmir-lib/src/vcs.rs
+++ hyrmir-lib/src/vcs.rs
@@ -2,6 +2,26 @@ use std::error::Error;
 use std::fmt::Debug;
 use crate::model::NormalizedPath;
 
+pub struct PathInfo {
+    id: usize,
+    path: NormalizedPath,
+}
+
+impl PathInfo {
+    pub fn new(id: usize, path: NormalizedPath) -> Self {
+        Self { id, path }
+    }
+    
+    pub fn get_id(&self) -> usize {
+        self.id
+    }
+    
+    pub fn get_path(&self) -> &NormalizedPath {
+        &self.path
+    }
+    
+}
+
 pub trait VCSError: Error {}
 
 pub trait VersionObject: Debug {
@@ -15,21 +35,41 @@ pub trait VCS: Debug + Clone + PartialEq + Eq {
     
     type VersionObject: VersionObject;
 
-    fn get_current_path(&self) -> Result<NormalizedPath, Self::VCSError>;
+    fn get_current_path(&self) -> Result<PathInfo, Self::VCSError>;
+
+    fn iter_concrete_paths(&self) -> impl Iterator<Item = Result<PathInfo, Self::VCSError>>;
 
     fn get_version(&self, version: &String) -> Result<Self::VersionObject, Self::VCSError>;
 
     fn version_exists_on_path(&self, path: &NormalizedPath, version: &String) -> Result<bool, Self::VCSError>;
-    
-    fn iter_concrete_paths(&self) -> impl Iterator<Item = Result<NormalizedPath, Self::VCSError>>;
 
     fn iter_versions(&self, path: &NormalizedPath) -> impl Iterator<Item=Result<Self::VersionObject, Self::VCSError>>;
+
+    fn get_status_without_current_info(&self, colored: bool) -> Result<String, Self::VCSError>;
 
     fn format_status_message(
         &self,
         current_path_msg: String,
-        pre_status: String,
-        post_status: String,
+        pre_status_msg: String,
+        post_status_msg: String,
         colored: bool,
-    ) -> Result<String, Self::VCSError>;
+    ) -> Result<String, Self::VCSError> {
+        let current_path_msg = if current_path_msg.is_empty() {
+            "".to_string()
+        } else {
+            format!("{current_path_msg}\n")
+        };
+        let pre_status_msg = if pre_status_msg.is_empty() {
+            "".to_string()
+        } else {
+            format!("{pre_status_msg}\n")
+        };
+        let native_status = self.get_status_without_current_info(colored)?;
+        let post_status_msg = if post_status_msg.is_empty() {
+            "".to_string()
+        } else {
+            format!("\n{post_status_msg}")
+        };
+        Ok(format!("{current_path_msg}{pre_status_msg}{native_status}{post_status_msg}"))
+    }
 }
